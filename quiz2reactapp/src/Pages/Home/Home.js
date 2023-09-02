2e9314f8a1eacec9db4b218587bc0c31b5a4e914
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-09-02 21:19:29 +0400

    Setup done.

--- quiz2reactapp/src/Pages/Home/Home.js
+++ quiz2reactapp/src/Pages/Home/Home.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import { Categories } from "../../Data";
-import { TextField, MenuItem } from "@mui/material";
+import { TextField, MenuItem, Button } from "@mui/material";
 
-const Home = () => {
+const Home = ({name, setName}) => {
+
+    const [category, setCategory] = useState("");
+    const [difficulty, setDifficulty] = useState("");
+    console.log(category, difficulty)
+
   return (
     <div className="content">
       <div className="setting">
@@ -14,11 +19,14 @@ const Home = () => {
             style={{ marginBottom: 25 }}
             label="Enter Your Name"
             variant="outlined"
+            onChange={(e)=> setName(e.target.value)}
           />
           <TextField
             select
             label="Select Category"
             variant="outlined"
+            value={category}
+            onChange={(e)=> setCategory(e.target.value)}
             style={{ marginBottom: 30 }}
           >
             {Categories.map((cat) => (
@@ -27,6 +35,27 @@ const Home = () => {
               </MenuItem>
             ))}
           </TextField>
+          <TextField
+            select
+            label="Select Difficulity"
+            variant="outlined"
+            value={difficulty}
+            onChange={(e)=> setDifficulty(e.target.value)}
+            style={{ marginBottom: 30 }}
+          >
+            <MenuItem key="Easy" value="easy">
+              Easy
+            </MenuItem>
+            <MenuItem key="Medium" value="medium">
+              Medium
+            </MenuItem>
+            <MenuItem key="Hard" value="hard">
+              Hard
+            </MenuItem>
+          </TextField>
+          <Button variant="contained" color="primary" size="large">
+            Start Quiz 
+          </Button>
         </div>
       </div>
 
