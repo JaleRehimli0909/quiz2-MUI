6b2abf78068b7e0503143cc825042b7540ca48a6
JaleRehimli0909 <rehimlijale0909@gmail.com>
2023-09-02 21:01:13 +0400

    Category section added

--- quiz2reactapp/src/Pages/Home/Home.js
+++ quiz2reactapp/src/Pages/Home/Home.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "./Home.css";
+import { Categories } from "../../Data";
+import { TextField, MenuItem } from "@mui/material";
 
 const Home = () => {
   return (
     <div className="content">
       <div className="setting">
         <span style={{ fontSize: 30 }}>Quiz Settings</span>
+
+        <div className="settings_select">
+          <TextField
+            style={{ marginBottom: 25 }}
+            label="Enter Your Name"
+            variant="outlined"
+          />
+          <TextField
+            select
+            label="Select Category"
+            variant="outlined"
+            style={{ marginBottom: 30 }}
+          >
+            {Categories.map((cat) => (
+              <MenuItem key={cat.category} value={cat.value}>
+                {cat.category}
+              </MenuItem>
+            ))}
+          </TextField>
+        </div>
       </div>
 
       <img src="./quiz.svg" className="banner" alt="quiz img" />
